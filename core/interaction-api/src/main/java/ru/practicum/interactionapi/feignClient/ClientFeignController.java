d6922ead8445871b9017603c0211ff61114ada02
Evstafev <tehnol25@yandex.ru>
2026-01-11 18:27:17 +0700

    ТЗ final microservices этап 3 исправлены замечания, исправлены ошибки на github

--- core/interaction-api/src/main/java/ru/practicum/interactionapi/feignClient/ClientFeignController.java
+++ core/interaction-api/src/main/java/ru/practicum/interactionapi/feignClient/ClientFeignController.java
@@ -1,7 +1,6 @@
 package ru.practicum.interactionapi.feignClient;
 
 import feign.FeignException;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.constraints.Positive;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
